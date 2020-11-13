39f2ef65614a02804d3445544cd5b221308d9769
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-11-13 22:10:00 +0900

    Created xcode project

---
+++ Shared/BokehCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BokehCalendarApp.swift
+//  Shared
+//
+//  Created by 鈴木 航 on 2021/04/12.
+//
+
+import SwiftUI
+
+@main
+struct BokehCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
